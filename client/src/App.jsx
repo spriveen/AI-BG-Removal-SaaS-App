fdbe9d290d0ce35f7363fc1387703364bdf94550
spriveen <rpeanuka@gmail.com>
2026-02-18 03:11:05 -0800

    update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
     <div>
-      hh
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/result' element={<Result/>} />
+      <Route path='/buy' element={<BuyCredit />} />
+     </Routes>
     </div>
   )
 }

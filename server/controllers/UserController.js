23817bc9383db334a95342db2c2c0ccf2ade3e88
spriveen <rpeanuka@gmail.com>
2026-02-25 19:41:51 -0800

    update server

---
+++ server/controllers/UserController.js
@@ -0,0 +1,8 @@
+
+
+//  API Controller Function to kanage clerk User With Database
+// http://localhost:4000/user/webhooks
+const clerkwebhooks = async (req, res) =>{
+
+}
+
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config' 
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 
 // App Config
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB()
 
 // initialize Middlewares
 app.use(express.json())
